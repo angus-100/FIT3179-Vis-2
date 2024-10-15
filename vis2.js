89907ec49c922688119677d66bec03304af32614
angus-100 <aplo0001@student.monash.edu>
2024-10-15 17:35:41 +1100

    Add files via upload

---
+++ vis2.js
@@ -0,0 +1,24 @@
+var vg_2 = "vis2_map.vg.json";
+vegaEmbed("#shark_map", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "vis2_scatter.vg.json";
+vegaEmbed("#bubble_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "sharkmap_tot.vg.json";
+vegaEmbed("#map_total", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "line_chart.vg.json";
+vegaEmbed("#line_chart", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "pie_chart.vg.json";
+vegaEmbed("#pie_chart", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
